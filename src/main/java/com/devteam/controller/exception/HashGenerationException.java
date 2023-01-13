d26e7602bfee343ffefe0ff3e19a2979db1428c4
KhanhLocVo <vokhanhloc1994@gmail.com>
2023-01-14 02:14:28 +0700

    # Fix Create Category (Update Database) & Encrypt Password & Handle Error 404, 500 - Complete Section 18

---
+++ src/main/java/com/devteam/controller/exception/HashGenerationException.java
@@ -0,0 +1,13 @@
+package com.devteam.controller.exception;
+
+public class HashGenerationException extends RuntimeException {
+
+	public HashGenerationException(String message) {
+		super(message);
+	}
+
+	public HashGenerationException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
